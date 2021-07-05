7993c2de09612993e787c343df28f553b7772ee9
xiaoas <xiaoqilin82@gmail.com>
2021-07-05 23:10:58 +0800

    init full workflow

---
+++ README.md
@@ -0,0 +1,3 @@
+# 极略自用DL
+
+This repository hosts the source and the supplementary code for building jlsgDL extension for noname kill.
---
+++ subCatalog.js
@@ -0,0 +1,9 @@
+extension["极略DL"]={
+	intro:"下载完整体极略自用",
+	author:"xiaoas",
+	netdisk:"",
+	forum:"",
+	size:"1K",
+	version:"1.0",
+	files:["extension.js"],
+};
